815a9c0643042885029f593bf6573761db7819b5
vjenacka <edins613@gmail.com>
2019-08-21 17:21:46 +0200

    Updates

---
+++ precache-manifest.025cca29bc58d719e0f987128f7a597a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef9ed9b2ec625183052e26a53997da22",
+    "url": "/FCC-Markdown-Previewer/index.html"
+  },
+  {
+    "revision": "0967e79346343f3e7b1b",
+    "url": "/FCC-Markdown-Previewer/static/css/main.1b9d565f.chunk.css"
+  },
+  {
+    "revision": "a30904cba32b554d36e7",
+    "url": "/FCC-Markdown-Previewer/static/js/2.dad938c6.chunk.js"
+  },
+  {
+    "revision": "0967e79346343f3e7b1b",
+    "url": "/FCC-Markdown-Previewer/static/js/main.cb8cf48d.chunk.js"
+  },
+  {
+    "revision": "c2b73810a5768a64fe73",
+    "url": "/FCC-Markdown-Previewer/static/js/runtime~main.e60a2e2c.js"
+  }
+]);
